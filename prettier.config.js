541f38bb57d7f0ff02e7bda3e6418f134e8e1634
mizuna toma <k4017135@gmail.com>
2026-05-06 13:58:56 +0900

    react-router-domのバージョンを7.15.0に更新し、prettierの設定ファイルを追加

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+  ],
+  importOrder: [
+    "^react",
+    "^next",
+    "<THIRD_PARTY_MODULES>",
+    "^@/app/(.*)",
+    "^@/components/(.*)",
+    "^@/utils/(.*)",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
