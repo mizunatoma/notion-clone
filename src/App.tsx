c25aaab3e7803041616111fbe7782753ae247d38
mizuna toma <k4017135@gmail.com>
2026-05-07 08:10:36 +0900

    axiosを依存関係に追加し、API用のaxiosインスタンスを作成。アプリのルーティングを更新し、レイアウトとページコンポーネントを追加。

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import NoteDetail from "./pages/NoteDetail";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
-  return (<div>Hello world</div>)
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <Routes>
+          <Route path="/signin" element={<Signin />}/>
+          <Route path="/signup" element={<Signup />}/>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />}/> 
+            <Route path="/notes/:id" element={<NoteDetail />}/>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
-
+export default App;
