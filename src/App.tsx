7fc1386b8f4ba83f25726f5183c54e146d0a5625
mizuna toma <k4017135@gmail.com>
2026-05-07 19:23:08 +0900

    認証インターセプターを追加し、アプリ起動時に現在のユーザーを取得する処理を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { useSetAtom } from "jotai";
+
 import Layout from "./Layout";
+import { authRepository } from "./modules/auth/auth.repository";
+import { currentUserAtom } from "./modules/auth/current-user.state";
 import Home from "./pages/Home";
 import NoteDetail from "./pages/NoteDetail";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const setCurrentUser = useSetAtom(currentUserAtom);
+
+  const fetchCurrentUser = async () => {
+    try {
+      const user = await authRepository.getCurrentUser();
+      setCurrentUser(user);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
+
+  if (isLoading) return <div />; // loading中は空のページを返す
+
   return (
     <BrowserRouter>
       <div className="app-container">
--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL
-const api =axios.create({ baseURL})
-api.defaults.headers.common['Content-Type'] = 'application/json'
+import { addAuthorizationHeaders } from "./interceptors/requests";
+
+const baseURL = import.meta.env.VITE_API_URL;
+const api = axios.create({ baseURL }); // axiosの慣用句「URLのプレフィックスを設定したaxiosを作る」
+api.defaults.headers.common["Content-Type"] = "application/json";
+api.interceptors.request.use(addAuthorizationHeaders); // インターセプターの利用設定
 
 export default api;
