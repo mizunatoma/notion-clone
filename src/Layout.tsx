ef6d1763847ff91bf3bf7e3df59febbfb920880d
mizuna toma <k4017135@gmail.com>
2026-05-09 10:06:33 +0900

    Layoutでノート一覧を初期取得しnoteStoreに反映する処理を実装

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 import { useAtomValue } from "jotai";
@@ -5,15 +6,31 @@ import { useAtomValue } from "jotai";
 import SearchModal from "./components/SearchModal";
 import SideBar from "./components/SideBar";
 import { currentUserAtom } from "./modules/auth/current-user.state";
+import { noteRepository } from "./modules/notes/note.repository";
+import { useNoteStore } from "./modules/notes/note.state";
 import "./styles/layout.css";
 
 export default function Layout() {
   const currentUser = useAtomValue(currentUserAtom);
+  const [isLoading, setIsLoading] = useState(false);
+  const noteStore = useNoteStore();
+
+  const fetchNotes = async () => {
+    setIsLoading(true);
+    const notes = await noteRepository.find();
+    noteStore.set(notes);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   if (!currentUser) return <Navigate to="/signin" replace />;
 
   return (
     <div className="layout-container">
-      <SideBar />
+      {!isLoading && <SideBar />}
       <main className="layout-main">
         <Outlet />
       </main>
--- src/modules/notes/note.repository.ts
+++ src/modules/notes/note.repository.ts
@@ -2,6 +2,11 @@ import api from "../../lib/api";
 import { Note } from "./note.entity";
 
 export const noteRepository = {
+  async find(): Promise<Note[]> {
+    const result = await api.get("/notes");
+    return result.data.notes.map((data: Note) => new Note(data));
+  },
+
   async create(params: { title?: string; parentId?: number }): Promise<Note> {
     const result = await api.post("/notes", {
       title: params.title,
